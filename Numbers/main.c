1f0ece2b05d83ae41ca6a05b2589179c0f7fe4e5
Madi-S <shaiken.m@mail.ru>
2022-07-24 18:22:04 +0600

    Numbers

---
+++ Numbers/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    // Math operations
+    printf("%f\n", 3.14 * 5.0 * 5.0);
+    printf("%d\n", 5 / 4);
+    printf("%f\n", 5 / 4.0);
+
+    // Math functions
+    printf("%f\n", pow(2, 10));
+    printf("%f\n", sqrt(490));
+    printf("%f\n", ceil(3.14));
+    printf("%f\n", floor(3.14));
+    printf("%d\n", abs(-12));
+    printf("%f\n", tan(1));
+    printf("%f\n", cos(1));
+    printf("%f\n", sin(1));
+    printf("%f\n", exp(1)); // e in power of
+
+    return 0;
+}
