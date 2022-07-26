bc9dca8cff854ce69d4dbf8dce0b5eeb9c360f26
Madi-S <shaiken.m@mail.ru>
2022-07-26 13:21:27 +0600

    For Loops

---
+++ For Loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nums[5] = {42, 91, 23, 100, 9};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", nums[i]);
+    }
+
+    return 0;
+}
