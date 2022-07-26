ff8e1af0bcaaad7de9dcc1933811217cc761e1ad
Madi-S <shaiken.m@mail.ru>
2022-07-26 13:22:50 +0600

    While Loops

---
+++ While Loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int index = 0;
+    while (index <= 5)
+    {
+        printf("%d\n", index);
+        index++;
+        // index = index + 1;
+    }
+    return 0;
+}
